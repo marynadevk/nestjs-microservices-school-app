f895b4a2399255ee60ad64a9ae723f4516d9ed82
Maryna Kucher <maryna.dev.k@gmail.com>
2024-07-19 16:43:00 +0300

    User model is created, ability to authenticate is implemented, project is set up

--- apps/account/src/app/auth/auth.service.ts
+++ apps/account/src/app/auth/auth.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDto } from './auth.controller';
+import { UserRepository } from '../user/repositories/user.repository';
+import { UserEntity } from '../user/entities/user.entity';
+import { UserRole } from '@school-project/interfaces';
+import { JwtService } from '@nestjs/jwt';
+
+const VALIDATION_ERROR = 'Credentials are not valid';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async signup({ email, password, displayName }: RegisterDto) {
+    const oldUser = await this.userRepository.findUserByEmail(email);
+    if (oldUser) {
+      throw new Error('User already exists');
+    }
+    const newUserEntity = await new UserEntity({
+      displayName,
+      passwordHash: '',
+      email,
+      role: UserRole.Student,
+    }).setPasswordHash(password);
+    const newUser = await this.userRepository.createUser(newUserEntity);
+    return { email: newUser.email };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userRepository.findUserByEmail(email);
+    if (!user) {
+      throw new Error(VALIDATION_ERROR);
+    }
+    const userEntity = new UserEntity(user);
+    const isPasswordValid = await userEntity.comparePassword(password);
+    if (!isPasswordValid) {
+      throw new Error(VALIDATION_ERROR);
+    }
+    return { id: user._id };
+  }
+
+  async login(id: string) {
+    return {
+      access_token: await this.jwtService.signAsync({ id }),
+    };
+  }
+}
---
+++ apps/account/src/app/user/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { IUser, UserRole } from '@school-project/interfaces';
+import * as bcrypt from 'bcrypt';
+
+export class UserEntity implements IUser {
+  _id?: string;
+  displayName?: string;
+  email: string;
+  passwordHash: string;
+  role: UserRole;
+
+  constructor(user: IUser) {
+    this._id = user._id;
+    this.passwordHash = user.passwordHash;
+    this.displayName = user.displayName;
+    this.email = user.email;
+    this.role = user.role;
+  }
+
+  public async setPasswordHash(password: string) {
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(password, salt);
+    return this;
+  }
+
+  public comparePassword(password: string) {
+    return bcrypt.compare(password, this.passwordHash);
+  }
+}
